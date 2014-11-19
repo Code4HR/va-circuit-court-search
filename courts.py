394fbfb529ecb3e27b4d04c91a5a2bca9a9b42cc
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2014-11-19 17:02:16 -0500

    Each request searches only one court
    Browser makes a ton of requests instead of making one and having
    content streamed. This allows us to throttle request rate. Should allow
    more simultaneous users per server and reduce load the site being
    scraped.

--- courts.py
+++ courts.py
@@ -2,8 +2,11 @@ import scraperwiki
 import cookielib, urllib, urllib2
 import json
 import os
+import flask
+import pickle
 from bs4 import BeautifulSoup
-from flask import Flask, Response, render_template
+from flask import Flask, session, Response, render_template
+
 app = Flask(__name__)
 
 def getCases(html, name, names):
@@ -31,16 +34,17 @@ def getCases(html, name, names):
     return False
 
 
-def search(opener, name, court, division):    
+def lookupCases(opener, name, court, division):    
     cases = []
     
     data = urllib.urlencode({'category': division,
         'lastName':name,
         'courtId':court,
         'submitValue':'N'})
-    search = opener.open('http://ewsocis1.courts.state.va.us/CJISWeb/Search.do', data)
-    done = getCases(BeautifulSoup(search.read()), name, cases)
-
+    searchResults = opener.open('http://ewsocis1.courts.state.va.us/CJISWeb/Search.do', data)
+    html = searchResults.read()
+    done = getCases(BeautifulSoup(html), name, cases)
+    
     data = urllib.urlencode({'courtId':court,
         'pagelink':'Next',
         'lastCaseProcessed':'',
@@ -55,57 +59,59 @@ def search(opener, name, court, division):
     
     count = 1
     while(not done and count < 6):
-        search = opener.open('http://ewsocis1.courts.state.va.us/CJISWeb/Search.do', data)
-        content = search.read()
-        done = getCases(BeautifulSoup(content), name, cases)
+        searchResults = opener.open('http://ewsocis1.courts.state.va.us/CJISWeb/Search.do', data)
+        html = searchResults.read()
+        done = getCases(BeautifulSoup(html), name, cases)
         count += 1
     return cases
 
-def start(name):
-    cj = cookielib.CookieJar()
-    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-    opener.addheaders = [
-        ('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11'),
-        ]
+@app.route("/search/<name>/court/<court>")
+def searchCourt(name, court):
+    if 'cookies' not in session:
+        return "Error. Please reload the page."
+    
+    cookieJar = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11')]
+    for cookie in pickle.loads(session['cookies']):
+        cookieJar.set_cookie(cookie)
+    
+    courtId = court[:3]
+    data = urllib.urlencode({'courtId':courtId,
+        'courtType':'C',
+        'caseType':'ALL',
+        'testdos':False,
+        'sessionCreate':'NEW',
+        'whichsystem':court})
+    place = opener.open('http://ewsocis1.courts.state.va.us/CJISWeb/MainMenu.do', data)
+    
+    courtSearch = {'name': court[5:], 'id': courtId}
+    courtSearch['criminalCases'] = lookupCases(opener, name.upper(), courtId, 'R')
+    courtSearch['civilCases'] = lookupCases(opener, name.upper(), courtId, 'CIVIL')
+    return render_template('court.html', court=courtSearch)
+    
+@app.route("/search/<name>")
+def search(name):
+    cookieJar = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cookieJar))
+    opener.addheaders = [('User-Agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11')]
     home = opener.open('http://ewsocis1.courts.state.va.us/CJISWeb/circuit.jsp')
+    
+    session['cookies'] = pickle.dumps(list(cookieJar))
+    
+    courts = []
     html = BeautifulSoup(home.read())
-
     for option in html.find_all('option'):
-        system = option['value']
-        courtId = system[:3]
-        courtName = system[5:]
+        courts.append({'fullName': option['value'], 'id': option['value'][:3], 'name': option['value'][5:]})
         
-        response = {'court': courtName}
-        
-        data = urllib.urlencode({'courtId':courtId,
-            'courtType':'C',
-            'caseType':'ALL',
-            'testdos':False,
-            'sessionCreate':'NEW',
-            'whichsystem':system})
-        place = opener.open('http://ewsocis1.courts.state.va.us/CJISWeb/MainMenu.do', data)
-        response['criminalCases'] = search(opener, name, courtId, 'R')
-        response['civilCases'] = search(opener, name, courtId, 'CIVIL')
-        yield response
+    return render_template('search.html', data={'name': name.upper(), 'courts': courts})
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
-def stream_template(template_name, **context):
-    # http://flask.pocoo.org/docs/patterns/streaming/#streaming-from-templates
-    app.update_template_context(context)
-    t = app.jinja_env.get_template(template_name)
-    rv = t.stream(context)
-    # uncomment if you don't need immediate reaction
-    ##rv.enable_buffering(5)
-    return rv
-
-@app.route('/search/<name>')
-def search_name(name):
-    return Response(stream_template('search.html', data={'name': name.upper(), 'courts': start(name.upper())}))
-
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
+    app.secret_key = 'doesnt-need-to-be-secret'
     app.run(host='0.0.0.0', port=port, debug=True)
