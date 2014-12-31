a819058a1fc783f0b7f4928e73735313cadf5379
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2014-12-30 23:50:11 -0500

    Process other two pages

--- scrape_by_case_number_html_to_db.py
+++ scrape_by_case_number_html_to_db.py
@@ -16,7 +16,7 @@ def get_data_from_table(case, table):
         case[name] = value
 
 def get_hearings_from_table(case, table):
-    case['hearings'] = []
+    case['Hearings'] = []
     
     rows = table.find_all('tr')
     col_names = [x.encode('ascii') for x in rows.pop(0).stripped_strings]
@@ -27,8 +27,8 @@ def get_hearings_from_table(case, table):
             hearing[col_names[i]] = col.get_text(strip=True) \
                                        .encode('ascii', 'ignore')
         hearing_dt = hearing['Date'] + hearing['Time']
-        hearing['datetime'] = datetime.strptime(hearing_dt, "%m/%d/%Y%I:%M%p")
-        case['hearings'].append(hearing)
+        hearing['Datetime'] = datetime.strptime(hearing_dt, "%m/%d/%Y%I:%M%p")
+        case['Hearings'].append(hearing)
 
 def value_to_datetime(case, name):
     case[name] = datetime.strptime(case[name], "%m/%d/%Y").date()
@@ -46,6 +46,8 @@ def process_case_details(case, html):
     get_data_from_table(case, sentencing_table)
     get_hearings_from_table(case, hearings_table)
     
+    # TODO: handle appeal table... not sure what that looks like
+    
     if 'Filed' in case:
         value_to_datetime(case, 'Filed')
     if 'OffenseDate' in case:
@@ -55,24 +57,63 @@ def process_case_details(case, html):
     if 'DispositionDate' in case:
         value_to_datetime(case, 'DispositionDate')
 
+def process_case_pleadings(case, html):
+    tables = html.find_all('table')
+    case_number_table = tables[3]
+    pleadings_table = tables[4]
+    
+    case['CaseNumber'] = list(case_number_table.stripped_strings)[1] \
+                                .encode('ascii', 'ignore')
+    case['Pleadings'] = []
+    rows = pleadings_table.find_all('tr')
+    col_names = [x.encode('ascii') for x in rows.pop(0).stripped_strings]
+    for row in rows:
+        pleadings = {}
+        for i, col in enumerate(row.find_all('td')):
+            pleadings[col_names[i]] = col.get_text(strip=True) \
+                                         .encode('ascii', 'ignore')
+        value_to_datetime(pleadings, 'Filed')
+        case['Pleadings'].append(pleadings)
+
+def process_case_services(case, html):
+    tables = html.find_all('table')
+    case_number_table = tables[3]
+    services_table = tables[4]
+    
+    case['CaseNumber'] = list(case_number_table.stripped_strings)[1] \
+                                .encode('ascii', 'ignore')
+    case['Services'] = []
+    rows = services_table.find_all('tr')
+    col_names = [x.encode('ascii').replace(' ', '') \
+                 for x in rows.pop(0).stripped_strings]
+    for row in rows:
+        services = {}
+        for i, col in enumerate(row.find_all('td')):
+            services[col_names[i]] = col.get_text(strip=True) \
+                                        .encode('ascii', 'ignore')
+        if services['HearDate'] != '':
+            value_to_datetime(services, 'HearDate')
+        if services['DateServed'] != '':
+            value_to_datetime(services, 'DateServed')
+        case['Services'].append(services)
+
 base_path = '../va-circuit-court-search-files/'
 locality_directories = os.walk(base_path).next()[1]
 for directory in locality_directories:
     filenames = os.listdir(base_path + directory)
     for filename in filenames:
+        print directory + ' - ' + filename
         cur_path = base_path + directory + '/' + filename
-        print cur_path
         with open(cur_path, 'r') as f:
             case = {}
             html = BeautifulSoup(f.read())
             
             if 'pleadings' in filename:
-                print 'process pleadings'
+                process_case_pleadings(case, html)
             elif 'services' in filename:
-                print 'process services'
+                process_case_services(case, html)
             else:
                 process_case_details(case, html)
             
-            print pprint(case)
     break
 print 'Done'
