b91b17c9f8154bfd636ead639e6ecf914f0cf4c8
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2015-01-02 23:04:01 -0500

    Get better at finding case numbers

--- scrape_by_case_number.py
+++ scrape_by_case_number.py
@@ -10,6 +10,27 @@ from time import sleep
 user_agent = u"Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; " + \
     u"rv:1.9.2.11) Gecko/20101012 Firefox/3.6.11"
 
+courts_with_fm_case_number = [
+    'Charlotte Circuit Court',
+    'Chesterfield Circuit Court',
+    'Page Circuit Court',
+    'Richmond City Circuit Court',
+    'Wise Circuit Court'
+]
+
+def get_case_number_with_mf(case_number):
+    return [
+        case_number[:4] + 'M' + case_number[5:],
+        case_number[:4] + 'F' + case_number[5:]
+    ]
+
+def get_case_number(court, case_count, charge_count):
+    case_number = 'CR14' + format(case_count, '06') + \
+        '-' + format(charge_count, '02')
+    if court in courts_with_fm_case_number:
+        return get_case_number_with_mf(case_number)
+    return [case_number]
+
 def getFilePath(court_name):
     court_name = court_name.replace(' ', '').replace('/', '')
     return '../va-circuit-court-search-files/' + court_name + '/'
@@ -37,10 +58,10 @@ def run():
         file_path = getFilePath(court['name'])
         if not os.path.exists(file_path):
             os.makedirs(file_path)
-        elif len(courts) > index + 1:
-            next_file_path = getFilePath(courts[index+1]['name'])
-            if os.path.exists(next_file_path):
-                continue
+        #elif len(courts) > index + 1:
+        #    next_file_path = getFilePath(courts[index+1]['name'])
+        #    if os.path.exists(next_file_path):
+        #        continue
         
         data = urllib.urlencode({
             'courtId': court['id'],
@@ -57,71 +78,75 @@ def run():
         case_count = -1
         file_list = os.listdir(file_path)
         if len(file_list) > 0:
-            case_count = int(file_list[-1][4:10]) - 1
+            case_count = int(file_list[-1][5:10]) - 1
         
         search_next_case = True
         while search_next_case:
             case_count += 1
-            case_number = 'CR14' + format(case_count, '06')
-            
-            search_next_specific_case = True
-            specific_case_count = -1
-            while search_next_specific_case:
-                specific_case_count += 1
-                if specific_case_count > 99:
+            search_next_charge = True
+            charge_count = -1
+            while search_next_charge:
+                charge_count += 1
+                if charge_count > 99:
                     break
-                specific_case_number = case_number + '-' + format(specific_case_count, '02')
+                case_numbers = get_case_number(court['name'], case_count, charge_count)
                 
-                # get case info
-                data = urllib.urlencode({
-                    'categorySelected': 'R',
-                    'caseNo': specific_case_number,
-                    'courtId': court['id'],
-                    'submitValue': ''})
-                cases_url = u"http://ewsocis1.courts.state.va.us/CJISWeb/CaseDetail.do"
-                case_details = opener.open(cases_url, data)
-                html = BeautifulSoup(case_details.read())
+                for case_number in case_numbers:
+                    # get case info
+                    data = urllib.urlencode({
+                        'categorySelected': 'R',
+                        'caseNo': case_number,
+                        'courtId': court['id'],
+                        'submitValue': ''})
+                    cases_url = u"http://ewsocis1.courts.state.va.us/CJISWeb/CaseDetail.do"
+                    case_details = opener.open(cases_url, data)
+                    html = BeautifulSoup(case_details.read())
                 
-                case_exists = html.find(text=re.compile('Case not found')) is None
+                    case_exists = html.find(text=re.compile('Case not found')) is None
+                    if case_exists:
+                        break
+                    else:
+                        print 'Could not find ' + case_number
+                
                 if case_exists:
-                    html_is_invalid = html.find(text=re.compile(specific_case_number)) is None
+                    html_is_invalid = html.find(text=re.compile(case_number)) is None
                     if html_is_invalid:
                         raise Exception('Case detail HTML is invalid')
                     
-                    print 'Found ' + specific_case_number
+                    print 'Found ' + case_number
                     sequential_cases_missed_count = 0
                     
-                    filename = file_path + specific_case_number + '.html'
+                    filename = file_path + case_number + '.html'
                     with open(filename, 'w') as text_file:
                         text_file.write(html.prettify().encode('UTF-8'))
                     
                     data = urllib.urlencode({
                         'categorySelected': 'R',
                         'caseStatus':'A',
-                        'caseNo': specific_case_number,
+                        'caseNo': case_number,
                         'courtId': court['id'],
                         'submitValue': 'P'})
                     case_details = opener.open(cases_url, data)
                     html = BeautifulSoup(case_details.read())
-                    html_is_invalid = html.find(text=re.compile(specific_case_number)) is None
+                    html_is_invalid = html.find(text=re.compile(case_number)) is None
                     if html_is_invalid:
                         raise Exception('Case detail pleadings HTML is invalid')
-                    filename = file_path + specific_case_number + '_pleadings.html'
+                    filename = file_path + case_number + '_pleadings.html'
                     with open(filename, 'w') as text_file:
                         text_file.write(html.prettify().encode('UTF-8'))
                     
                     data = urllib.urlencode({
                         'categorySelected': 'R',
                         'caseStatus':'A',
-                        'caseNo': specific_case_number,
+                        'caseNo': case_number,
                         'courtId': court['id'],
                         'submitValue': 'S'})
                     case_details = opener.open(cases_url, data)
                     html = BeautifulSoup(case_details.read())
-                    html_is_invalid = html.find(text=re.compile(specific_case_number)) is None
+                    html_is_invalid = html.find(text=re.compile(case_number)) is None
                     if html_is_invalid:
                         raise Exception('Case detail services HTML is invalid')
-                    filename = file_path + specific_case_number + '_services.html'
+                    filename = file_path + case_number + '_services.html'
                     with open(filename, 'w') as text_file:
                         text_file.write(html.prettify().encode('UTF-8'))
                     
@@ -129,10 +154,10 @@ def run():
                     opener.open(place_url, data)
                     sleep(1)
                 else:
-                    print 'Could not find ' + specific_case_number
-                    if specific_case_count > 0:
-                        search_next_specific_case = False
-                    if specific_case_count == 1:
+                    print 'Could not find ' + case_number
+                    if charge_count > 0:
+                        search_next_charge = False
+                    if charge_count == 1:
                         sequential_cases_missed_count += 1
                         if sequential_cases_missed_count > 9:
                             search_next_case = False
