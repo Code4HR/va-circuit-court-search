88bb4cfba790af6527619e31890de11144b1b391
Ben Schoenfeld <ben.schoenfeld@gmail.com>
2015-01-25 09:11:27 -0500

    Save case numbers in db

--- collect_case_numbers.py
+++ collect_case_numbers.py
@@ -1,5 +1,6 @@
 import cookielib
 import os
+import pymongo
 import re
 import sys
 import urllib
@@ -29,7 +30,7 @@ def get_list_of_courts(opener):
         })
     return courts
 
-def get_case_numbers(opener, court, name):
+def get_case_numbers(opener, db, court, name):
     courtId = court[:3]
     data = urllib.urlencode({
         'courtId': courtId,
@@ -41,7 +42,6 @@ def get_case_numbers(opener, court, name):
     place_url = u"http://ewsocis1.courts.state.va.us/CJISWeb/MainMenu.do"
     opener.open(place_url, data)
     
-    cases = []
     data = urllib.urlencode({
         'category': 'R',
         'lastName': name,
@@ -50,7 +50,7 @@ def get_case_numbers(opener, court, name):
     search_url = u"http://ewsocis1.courts.state.va.us/CJISWeb/Search.do"
     search_results = opener.open(search_url, data)
     html = search_results.read()
-    done = get_cases(BeautifulSoup(html), cases)
+    final_case = get_cases(BeautifulSoup(html), court, db)
 
     data = urllib.urlencode({
         'courtId': courtId,
@@ -65,27 +65,49 @@ def get_case_numbers(opener, court, name):
         'searchType': '',
         'emptyList': ''})
 
-    count = 1
-    while(not done and count < 10):
+    #count = 1
+    final_case_prev = None
+    while(final_case != final_case_prev):
         search_results = opener.open(search_url, data)
         html = search_results.read()
-        done = get_cases(BeautifulSoup(html), cases)
-        count += 1
-    return cases
+        final_case_prev = final_case
+        final_case = get_cases(BeautifulSoup(html), court, db)
+        #count += 1
 
-def get_cases(html, names):
+def get_cases(html, court, db):
+    final_case = None
     for row in html.find(class_="nameList").find_all('tr'):
         cols = row.find_all('td')
         if len(cols) > 1:
             case_number = cols[0].span.a.string.strip()
             name = cols[1].string.strip()
-            names.append({
-                'case_number': case_number,
-                'name': name
-            })
+            db.case_numbers.update({
+                'court': court,
+                'case_number': case_number
+            }, {
+                '$set': {
+                    'court': court,
+                    'case_number': case_number,
+                    'name': name,
+                    'charge': cols[2].string.strip(),
+                    'date': cols[3].string.strip(),
+                    'status': cols[4].string.strip()
+                }
+            }, upsert = True)
             print case_number, name
-    return False
+            final_case = case_number
+    return final_case
 
+client = pymongo.MongoClient(os.environ['MONGO_URI'])
+db = client.va_circuit_court_case_numbers
 opener = get_opener()
 courts = get_list_of_courts(opener)
-get_case_numbers(opener, courts[0]['fullName'], 'A')
+
+for court in courts:
+    last_record = db.case_numbers.find_one({'court': court['fullName']}, \
+                        sort=[('name', pymongo.DESCENDING)])
+    last_name = 'A'
+    if last_record is not None:
+        last_name = last_record['name']
+    print court['fullName'], last_name
+    get_case_numbers(opener, db, court['fullName'], last_name)
